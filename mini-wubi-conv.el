fe0c8ea6859240f5ef6d74ef9e4157b7005c1ce0
luishen <luishen@vip.qq.com>
2017-09-14 10:29:44 +0800

    change mini-wubi-rules default find file directory and save directory

--- mini-wubi-conv.el
+++ mini-wubi-conv.el
@@ -1,5 +1,8 @@
 ;;; mini-wubi-conv.el  -*- coding: utf-8 -*-
+(defconst mini-wubi-rules-save-dir (file-name-directory (or load-file-name buffer-file-name)))
+(defconst mini-wubi-rules-filename (expand-file-name "mini-wubi-rules.el" (file-name-as-directory mini-wubi-rules-save-dir)))
 
+;;;###autoload
 (defun mini-wubi-data-line-to-quail-rules-line (string)
   (let* ((input-line-data (split-string string " " t))
 	 (key-seqs (car input-line-data))
@@ -9,21 +12,27 @@
 	       trans)
       (setq output-line-data (cons key-seqs (vconcat trans))))))
 
-(defun mini-wubi-convert-data-file-to-quail-rules-file (filename)
-  (interactive "FFind wubi data file: ")
-  (with-temp-buffer
-    (message "Reading %s..." filename)
-    (insert-file-contents filename)
-    (let ((input-datas (split-string (buffer-string) "\n" t)))
-      (with-temp-file "mini-wubi-rules.el"
-	(message "Converting...")
-	(insert ";; -*-no-byte-compile: t; -*-\n")
-	(insert "(quail-define-rules\n")
-	(dolist (output-line-data (mapcar 'mini-wubi-data-line-to-quail-rules-line input-datas))
-	  (insert (format "(%-6S %S)" (car output-line-data) (cdr output-line-data)))
-	  (insert "\n"))
-	(insert ")\n")))
-    (message "Finished")))
+;;;###autoload
+(defun mini-wubi-convert-data-file-to-quail-rules-file ()
+  (interactive)
+  (let ((default-directory mini-wubi-rules-save-dir))
+    (call-interactively
+     (lambda (filename)
+       (interactive "fFind wubi data file: ")
+       (with-temp-buffer
+         (message "Reading %s ..." filename)
+         (insert-file-contents filename)
+         (let ((input-datas (split-string (buffer-string) "\n" t)))
+           (with-temp-file mini-wubi-rules-filename
+	           (message "Converting to %s ..." mini-wubi-rules-filename)
+	           (insert ";; -*-no-byte-compile: t; -*-\n")
+	           (insert "(quail-define-rules\n")
+	           (dolist (output-line-data (mapcar 'mini-wubi-data-line-to-quail-rules-line input-datas))
+	             (insert (format "(%-6S %S)" (car output-line-data) (cdr output-line-data)))
+	             (insert "\n"))
+	           (insert ")\n")))
+         (message "Finished"))))))
 
 (provide 'mini-wubi-conv)
+
 ;;; mini-wubi-conv.el ends here
