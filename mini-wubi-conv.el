ade1f8dc55bdb3e2982a2d5d702465640c66421e
luishen <luishen@vip.qq.com>
2017-09-18 11:36:56 +0800

    add license

--- mini-wubi-conv.el
+++ mini-wubi-conv.el
@@ -1,4 +1,30 @@
-;;; mini-wubi-conv.el  -*- coding: utf-8 -*-
+;;; mini-wubi-conv.el --- convert plan text file  to quail rules file -*- lexical-binding: t; -*-
+;;; -*- coding: utf-8 -*-
+
+;; Copyright (C) 2017 SuJiKiNen
+
+;; Author: SuJiKiNen <SuJiKiNen@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (defconst mini-wubi-rules-save-dir (file-name-directory (or load-file-name buffer-file-name)))
 (defconst mini-wubi-rules-filename (expand-file-name "mini-wubi-rules.el" (file-name-as-directory mini-wubi-rules-save-dir)))
 
--- mini-wubi.el
+++ mini-wubi.el
@@ -1,4 +1,30 @@
-;;; mini-wubi.el  -*- coding: utf-8 -*-
+;;; mini-wubi.el --- A simple Chinese wubi input method inside Emacs -*- lexical-binding: t; -*-
+;;; -*- coding: utf-8 -*-
+
+;; Copyright (C) 2017 SuJiKiNen
+
+;; Author: SuJiKiNen <SuJiKiNen@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'quail)
 
 (defvar mini-wubi-rules-loaded-flag nil "flag that tell whether mini-wubi-rules loaed or not.")
@@ -74,4 +100,5 @@
  mini-wubi-simple)
 
 (provide 'mini-wubi)
+
 ;;; mini-wubi.el ends here
